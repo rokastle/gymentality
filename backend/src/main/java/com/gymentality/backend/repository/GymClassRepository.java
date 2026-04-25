34c07c55abe8a1a654c4563106cb8df6ba22df06
rokastle <robertocastillojimenez@gmail.com>
2026-04-25 14:10:36 +0200

    Remove duplicated GymClass domain files

--- backend/src/main/java/com/gymentality/backend/repository/GymClassRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gymentality.backend.repository;
-
-import com.gymentality.backend.entity.GymClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GymClassRepository extends JpaRepository<GymClass, Long> {
-}
